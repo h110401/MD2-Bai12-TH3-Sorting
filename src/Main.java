598adc8181cb52cae311e02f91259afa4de985cd
h110401 <hung110401@gmail.com>
2022-08-09 02:11:27 +0700

    th3

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student("Kien", 30, "HT");
+        Student student1 = new Student("Nam", 26, "HN");
+        Student student2 = new Student("Anh", 38, "HT");
+        Student student3 = new Student("Tung", 38, "HT");
+
+        List<Student> students = new ArrayList<>();
+        students.add(student);
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+        Collections.sort(students);
+        for (Student s : students) {
+            System.out.println(s);
+        }
+
+        AgeComparator ageComparator = new AgeComparator();
+        Collections.sort(students, ageComparator);
+        System.out.println("Compare age");
+        for (Student s : students) {
+            System.out.println(s);
+        }
+    }
+}
